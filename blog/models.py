87997be268c2d2b69ffd7cbed577ade0b80aff32
bvishnuvamsi <bvamsisrinivas1@gmail.com>
2019-11-03 15:23:41 +0530

    home

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+#create a blog models
+#title
+#pub_date
+#body(text)
+#image
+
+#Add the blog app to settings
+#create a migration
+#migrate
+#Add to the admin
+
+
+class Blog(models.Model):
+    title=models.CharField(max_length=255)
+    pub_date=models.DateTimeField()
+    body=models.TextField()
+    image=models.ImageField(upload_to='images/')
+
